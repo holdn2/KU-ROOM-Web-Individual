3bca6909f9565787e79b16a570e968da8fca33fc
holdn2 <yoocy01@gmail.com>
2025-03-03 02:01:32 +0900

    feat: 약관 동의 구현중

--- src/pages/Signup/Agreement.tsx
+++ src/pages/Signup/Agreement.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import styles from "./Agreement.module.css";
+import TopIcon from "../../components/TopIcon";
 
 type Props = {};
 
 const Agreement = (props: Props) => {
-  return <div>Agreement</div>;
+  return (
+    <div className={styles.PageWrapper}>
+      <div className={styles.MainArea}>
+        <TopIcon />
+        <h1 className={styles.PageTitle}>
+          회원가입을 위한
+          <br />
+          약관에 동의해주세요
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Agreement;
