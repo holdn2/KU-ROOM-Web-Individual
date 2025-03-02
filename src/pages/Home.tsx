2770f75f86aad710cc4c91e86130537386c5a89a
holdn2 <yoocy01@gmail.com>
2025-03-02 14:52:11 +0900

    test: 배포 테스트

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,13 @@
 // 홈 페이지
 
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/login");
+  }, []);
   return <div>Home</div>;
 };
 
