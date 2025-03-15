6b4b8718af62b7e251c26492d16638fda630ffef
holdn2 <yoocy01@gmail.com>
2025-03-15 11:11:00 +0900

    feat : 헤더 컴포넌트 구현

--- src/pages/Notice.tsx
+++ src/pages/Notice.tsx
@@ -1,10 +1,12 @@
 // 공지사항 페이지
 import BottomBar from "../components/BottomBar/BottomBar";
+import Header from "../components/Header/Header";
 
 const Notice = () => {
   return (
     <div>
-      공지사항
+      <Header>공지사항</Header>
+      <div style={{ marginTop: "124px" }}>내용 부분</div>
       <BottomBar />
     </div>
   );
