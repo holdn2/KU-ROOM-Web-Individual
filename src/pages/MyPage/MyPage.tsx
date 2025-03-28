44120424e037481d03574e4afe58e46625cefb91
holdn2 <yoocy01@gmail.com>
2025-03-28 13:25:22 +0900

    feat : BeatLoader사용한 로딩 애니메이션 추가.

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -5,8 +5,29 @@ import Header from "../../components/Header/Header";
 import MyProfileComponent from "../../components/MyProfile/MyProfileComponent";
 import ProfileSection from "../../components/MyProfile/ProfileSection";
 import { MyPageSectionData } from "../../constants/sectionDatas";
+import { useEffect, useState } from "react";
+import { BeatLoader } from "react-spinners";
 
 const MyPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // ✅ 여기에 실제 API 로딩 or 이미지 로딩 조건으로 변경 가능
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000); // 1초 로딩 시뮬레이션
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className={styles.MyPageLoadingWrapper}>
+        <BeatLoader color="#009733" size={18} margin={4} />
+      </div>
+    );
+  }
+
   return (
     <div>
       <Header>마이페이지</Header>
