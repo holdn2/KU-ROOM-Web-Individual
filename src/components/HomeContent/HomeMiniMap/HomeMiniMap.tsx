ad819c409b6f9b78b726cb831decd3878642b3b0
holdn2 <yoocy01@gmail.com>
2025-04-15 11:03:08 +0900

    refactor : 홈 미니맵 로직 utils로 분리

--- src/components/HomeContent/HomeMiniMap/HomeMiniMap.tsx
+++ src/components/HomeContent/HomeMiniMap/HomeMiniMap.tsx
@@ -3,6 +3,7 @@ import styles from "./HomeMiniMap.module.css";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import KuroomMap from "../../Map/KuroomMap";
+import { isMyLocationInSchool } from "./HomeMiniMapUtils";
 
 interface HomeMiniMapProps {
   isSharedLocation: boolean;
@@ -16,74 +17,9 @@ const HomeMiniMap: React.FC<HomeMiniMapProps> = ({
   const navigate = useNavigate();
   const [isInSchool, setIsInSchool] = useState(false);
 
-  // 건국대 범위 사각형으로 지정
-  const schoolRange = [
-    [127.070767, 37.540034],
-    [127.0742, 37.5472],
-    [127.083, 37.5451],
-    [127.0819, 37.537],
-  ];
-
-  // 테스트용 범위
-  // const schoolRange = [
-  //   [127.1037, 37.6159],
-  //   [127.1053, 37.6213],
-  //   [127.1094, 37.62],
-  //   [127.1075, 37.6144],
-  // ];
-
-  // 각 좌표 안쪽에 있는지 검증하는 로직
-  const isPointInSchool = (
-    userLng: number,
-    userLat: number,
-    schoolRange: number[][]
-  ) => {
-    let inside = false;
-    for (
-      let i = 0, j = schoolRange.length - 1;
-      i < schoolRange.length;
-      j = i++
-    ) {
-      const xi = schoolRange[i][0],
-        yi = schoolRange[i][1];
-      const xj = schoolRange[j][0],
-        yj = schoolRange[j][1];
-
-      const intersect =
-        yi > userLat !== yj > userLat &&
-        userLng < ((xj - xi) * (userLat - yi)) / (yj - yi) + xi;
-
-      if (intersect) inside = !inside;
-    }
-    return inside;
-  };
-
   // 현재 위치가 학교 내부 인지 검증
   useEffect(() => {
-    if (!navigator.geolocation) return;
-
-    // watchPosition으로 이동할 때마다 검증
-    const watchId = navigator.geolocation.watchPosition(
-      (position) => {
-        const userLat = position.coords.latitude;
-        const userLng = position.coords.longitude;
-
-        const inside = isPointInSchool(userLng, userLat, schoolRange);
-        setIsInSchool(inside); // 밖으로 나가면 false 처리됨
-      },
-      (err) => {
-        console.warn("위치 정보를 가져올 수 없습니다.", err);
-      },
-      {
-        enableHighAccuracy: true,
-        maximumAge: 5000,
-        timeout: 10000,
-      }
-    );
-
-    return () => {
-      navigator.geolocation.clearWatch(watchId); // 컴포넌트 unmount 시 정리
-    };
+    isMyLocationInSchool(setIsInSchool);
   }, []);
 
   const handleSeeMap = () => {
---
+++ src/components/HomeContent/HomeMiniMap/HomeMiniMapUtils.ts
@@ -0,0 +1,65 @@
+// 건국대 범위 사각형으로 지정
+const schoolRange = [
+  [127.070767, 37.540034],
+  [127.0742, 37.5472],
+  [127.083, 37.5451],
+  [127.0819, 37.537],
+];
+
+// 테스트용 범위
+// const schoolRange = [
+//   [127.1037, 37.6159],
+//   [127.1053, 37.6213],
+//   [127.1094, 37.62],
+//   [127.1075, 37.6144],
+// ];
+
+// 각 좌표(4개) 안에 있는지 검증하는 로직
+export function isPointInSchool(
+  userLng: number,
+  userLat: number,
+  schoolRange: number[][]
+) {
+  let inside = false;
+  for (let i = 0, j = schoolRange.length - 1; i < schoolRange.length; j = i++) {
+    const xi = schoolRange[i][0],
+      yi = schoolRange[i][1];
+    const xj = schoolRange[j][0],
+      yj = schoolRange[j][1];
+
+    const intersect =
+      yi > userLat !== yj > userLat &&
+      userLng < ((xj - xi) * (userLat - yi)) / (yj - yi) + xi;
+
+    if (intersect) inside = !inside;
+  }
+  return inside;
+}
+
+// 내 위치를 추적하여 학교 내부인지 검증하는 로직
+export function isMyLocationInSchool(setIsInSchool: (value: boolean) => void) {
+  if (!navigator.geolocation) return;
+
+  // watchPosition으로 이동할 때마다 검증
+  const watchId = navigator.geolocation.watchPosition(
+    (position) => {
+      const userLat = position.coords.latitude;
+      const userLng = position.coords.longitude;
+
+      const inside = isPointInSchool(userLng, userLat, schoolRange);
+      setIsInSchool(inside); // 밖으로 나가면 false 처리됨
+    },
+    (err) => {
+      console.warn("위치 정보를 가져올 수 없습니다.", err);
+    },
+    {
+      enableHighAccuracy: true,
+      maximumAge: 5000,
+      timeout: 5000,
+    }
+  );
+
+  return () => {
+    navigator.geolocation.clearWatch(watchId); // 컴포넌트 unmount 시 정리
+  };
+}
