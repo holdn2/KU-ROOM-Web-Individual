257e25441b85a972dc34653cee424e3f6b758de5
holdn2 <yoocy01@gmail.com>
2025-05-05 12:42:35 +0900

    feat : 홈 미니맵 로직 및 텍스트 수정

--- src/components/HomeContent/HomeMiniMap/HomeMiniMap.tsx
+++ src/components/HomeContent/HomeMiniMap/HomeMiniMap.tsx
@@ -28,20 +28,44 @@ const HomeMiniMap: React.FC<HomeMiniMapProps> = ({
   const handleShareLocation = () => {
     console.log("내 위치 공유하기");
     setIsSharedLocation(true);
+    // 서버에 현재 위치 보내기, 공유 상태 보내기
   };
   const handleUnshareLocation = () => {
     console.log("위치 공유 해제하기");
     setIsSharedLocation(false);
+    // 공유 상태 보내기
   };
   return (
     // 내 위치 불러오기 전에 렌더링 안되게. 로딩 표시라도 추가
-    <>
-      {isInSchool && (
+    isInSchool ? (
+      isSharedLocation ? (
         <div className={styles.HomeMiniMapBackground}>
           <div className={styles.HomeMiniMapWrapper}>
             <div className={styles.MiniMapTextContainer}>
               <h1 className={styles.MiniMapBoldText}>
-                현재 학교 내부에 있어요!
+                현재 상허기념박물관에 계시네요!
+              </h1>
+              <span className={styles.MiniMapNormalText}>
+                장소를 이동할 땐 위치 공유를 해제해주세요 :)
+              </span>
+            </div>
+            <div className={styles.HomeMiniMap} onClick={handleSeeMap}>
+              <KuroomMap
+                height="180px"
+                isTracking={true}
+                draggable={false}
+                zoomable={false}
+              />
+            </div>
+            <Button onClick={handleUnshareLocation}>위치 공유 해제하기</Button>
+          </div>
+        </div>
+      ) : (
+        <div className={styles.HomeMiniMapBackground}>
+          <div className={styles.HomeMiniMapWrapper}>
+            <div className={styles.MiniMapTextContainer}>
+              <h1 className={styles.MiniMapBoldText}>
+                현재 건국대학교에 계신가요?
               </h1>
               <span className={styles.MiniMapNormalText}>
                 내 위치를 친구들에게 공유해보세요!
@@ -55,17 +79,13 @@ const HomeMiniMap: React.FC<HomeMiniMapProps> = ({
                 zoomable={false}
               />
             </div>
-            {isSharedLocation ? (
-              <Button onClick={handleUnshareLocation}>
-                위치 공유 해제하기
-              </Button>
-            ) : (
-              <Button onClick={handleShareLocation}>내 위치 공유하기</Button>
-            )}
+            <Button onClick={handleShareLocation}>내 위치 공유하기</Button>
           </div>
         </div>
-      )}
-    </>
+      )
+    ) : (
+      <></>
+    )
   );
 };
 
