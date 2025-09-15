e86f4fe621c7ee3608b00d2149e04ea8975c5e38
holdn2 <yoocy01@gmail.com>
2025-09-15 13:38:17 +0900

    feat: 챗봇 메인 페이지 퍼블리싱 완료

--- src/pages/Chatbot/ChatbotMain.tsx
+++ src/pages/Chatbot/ChatbotMain.tsx
@@ -1,8 +1,68 @@
+import { ChangeEvent, useState } from "react";
+
+import SendIcon from "@assets/icon/send.svg";
+import ChatIcon from "@assets/icon/chat.svg";
+import DisabledSendIcon from "@assets/icon/send_disabled.svg";
+import Header from "@components/Header/Header";
+
+import styles from "./ChatbotMain.module.css";
+import { mockRecommendation } from "./constant/mock-recommendation";
 
 const ChatbotMain = () => {
-  return (
-    <div className={""}>ChatbotMain</div>
-  )
-}
+  const [inputText, setInputText] = useState("");
 
-export default ChatbotMain
+  const handleChangeInputText = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  };
+
+  const handleSendMessage = () => {
+    console.log(inputText);
+  };
+
+  return (
+    <div>
+      <Header>챗봇</Header>
+      <div className={styles.ChatbotMainWrapper}>
+        <div className={styles.SearchSection}>
+          <span className={styles.ChatbotDescription}>
+            건국대학교 공지사항을
+            <br />
+            <span className={styles.DescriptionHighlight}>AI 챗봇</span>
+            으로 쉽게 확인해보세요!
+          </span>
+          <div className={styles.InputContainer}>
+            <input
+              className={styles.InputTextArea}
+              type="text"
+              value={inputText}
+              onChange={(e) => handleChangeInputText(e)}
+              placeholder="무엇이든 물어보세요"
+            />
+            <button
+              className={styles.SendIcon}
+              onClick={handleSendMessage}
+              disabled={!inputText}
+            >
+              <img src={inputText ? SendIcon : DisabledSendIcon} alt="보내기" />
+            </button>
+          </div>
+        </div>
+        <div className={styles.RecommendationSection}>
+          {mockRecommendation.map((item) => (
+            <button key={item.id} className={styles.RecommendationContainer}>
+              <img src={ChatIcon} />
+              <div className={styles.RecommendationText}>
+                <span className={styles.QuestionDesc}>{item.description}</span>
+                <span className={styles.RecommendationQuestion}>
+                  {item.question}
+                </span>
+              </div>
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatbotMain;
---
+++ src/pages/Chatbot/constant/mock-recommendation.ts
@@ -0,0 +1,17 @@
+export const mockRecommendation = [
+  {
+    id: 1,
+    question: "졸업식 언제인지 알려줘",
+    description: "학교 행사 정보 확인하기",
+  },
+  {
+    id: 2,
+    question: "컴퓨터공학과 졸업요건이 뭐야?",
+    description: "졸업요건 확인하기",
+  },
+  {
+    id: 3,
+    question: "장학금 지급일정 알려줘",
+    description: "장학금 정보 확인하기",
+  },
+];
