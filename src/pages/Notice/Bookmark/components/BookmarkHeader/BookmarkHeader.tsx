244080d3d9dd168d3d7ffce522df165ee13ee9f5
holdn2 <yoocy01@gmail.com>
2025-09-30 17:55:26 +0900

    fix: 빌드 오류 수정

--- src/pages/Notice/Bookmark/components/BookmarkHeader/BookmarkHeader.tsx
+++ src/pages/Notice/Bookmark/components/BookmarkHeader/BookmarkHeader.tsx
@@ -1,4 +1,3 @@
-import { useNavigate } from "react-router-dom";
 import Header from "@components/Header/Header";
 import styles from "./BookmarkHeader.module.css";
 
@@ -7,16 +6,6 @@ interface BookmarkHeaderProps {
 }
 
 export const BookmarkHeader = ({ title = "북마크" }: BookmarkHeaderProps) => {
-  const navigate = useNavigate();
-
-  const handleGoBack = (): void => {
-    if (window.history.length > 1) {
-      navigate(-1);
-    } else {
-      navigate("/");
-    }
-  };
-
   return (
     <div className={styles["fixed-header"]}>
       <Header>{title}</Header>
