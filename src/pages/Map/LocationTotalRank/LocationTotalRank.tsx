4f87bc11dd666e9d6ebe1ef32a862f7e0c7ceb76
holdn2 <yoocy01@gmail.com>
2025-11-01 10:34:36 +0900

    feat: 1, 2, 3등에 대한 ui 구현

--- src/pages/Map/LocationTotalRank/LocationTotalRank.tsx
+++ src/pages/Map/LocationTotalRank/LocationTotalRank.tsx
@@ -3,9 +3,9 @@ import { useLocation, useParams } from "react-router-dom";
 
 import Header from "@/shared/components/Header/Header";
 import { getLocationTotalRank } from "@/apis/map";
-// import Rank1Icon from "@assets/icon/ranking/rank1.png";
-// import Rank2Icon from "@assets/icon/ranking/rank2.png";
-// import Rank3Icon from "@assets/icon/ranking/rank3.png";
+import Rank1Icon from "@assets/icon/ranking/rank1.png";
+import Rank2Icon from "@assets/icon/ranking/rank2.png";
+import Rank3Icon from "@assets/icon/ranking/rank3.png";
 
 import styles from "./LocationTotalRank.module.css";
 
@@ -23,10 +23,20 @@ const LocationTotalRank = () => {
   const [totalRankData, setTotalRankData] = useState<RankDataType[] | null>(
     null
   );
+  const [modalState, setModalState] = useState(false);
+
+  const handleOpenModal = (rankData: RankDataType) => {
+    setModalState(true);
+    alert(`${rankData.ranking}등에 대한 모달 오픈`);
+    console.info(modalState);
+  };
+  // const handleCloseModal = () => {
+  //   setModalState(false);
+  // };
 
   const getCurrentLocationTotalRank = async () => {
     try {
-      const response = await getLocationTotalRank(Number(placeId), 1, 5);
+      const response = await getLocationTotalRank(Number(placeId), 1, 10);
       setTotalRankData(response);
     } catch (error) {
       console.error("현재 위치 랭킹 조회 시 클라측 오류 : ", error);
@@ -34,30 +44,91 @@ const LocationTotalRank = () => {
   };
   useEffect(() => {
     getCurrentLocationTotalRank();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
     console.log(totalRankData);
   }, [totalRankData]);
 
+  const rankIconBg = (ranking: number) => {
+    switch (ranking) {
+      case 1:
+        return "#F8DDDC";
+      case 2:
+        return "#DEE9FB";
+      case 3:
+        return "#FCEED7";
+    }
+  };
+  const rankIcon = (ranking: number) => {
+    switch (ranking) {
+      case 1:
+        return Rank1Icon;
+      case 2:
+        return Rank2Icon;
+      case 3:
+        return Rank3Icon;
+    }
+  };
+
   return (
     <div>
       <Header>{placeName}</Header>
       <div className={styles.TotalRankingPageWrapper}>
-        <div className={styles.LowRankWrapper}>
-          {totalRankData &&
-            totalRankData.map((rankData) => (
-              <div key={`${rankData.nickname}`} className={styles.LowRanksData}>
-                <span className={styles.LowRanking}>{rankData.ranking}</span>
-                <div className={styles.LowRanksInfo}>
-                  <span className={styles.RankerName}>{rankData.nickname}</span>
-                  <span className={styles.SharingCount}>
-                    {rankData.sharingCount}회
-                  </span>
+        {totalRankData && (
+          <>
+            <div className={styles.TopRankWrapper}>
+              {totalRankData.slice(0, 3).map((rankData) => (
+                <div key={rankData.ranking} className={styles.TopRanksData}>
+                  <div
+                    className={styles.RankIconSection}
+                    style={{
+                      backgroundColor: `${rankIconBg(rankData.ranking)}`,
+                    }}
+                  >
+                    <img
+                      className={styles.RankIcon}
+                      src={rankIcon(rankData.ranking)}
+                      alt="등수 아이콘"
+                    />
+                  </div>
+                  <div className={styles.TopRanksInfo}>
+                    <div className={styles.TopRanksNameWrapper}>
+                      <span className={styles.RankerName}>
+                        {rankData.nickname[0]}
+                      </span>
+                      <button
+                        className={styles.RestCount}
+                        onClick={() => handleOpenModal(rankData)}
+                      >
+                        외 {rankData.nickname.length - 1}명
+                      </button>
+                    </div>
+                    <span className={styles.SharingCount}>
+                      {rankData.sharingCount}회
+                    </span>
+                  </div>
                 </div>
-              </div>
-            ))}
-        </div>
+              ))}
+            </div>
+            <div className={styles.LowRankWrapper}>
+              {totalRankData.slice(3).map((rankData) => (
+                <div key={rankData.ranking} className={styles.LowRanksData}>
+                  <span className={styles.LowRanking}>{rankData.ranking}</span>
+                  <div className={styles.LowRanksInfo}>
+                    <span className={styles.RankerName}>
+                      {rankData.nickname}
+                    </span>
+                    <span className={styles.SharingCount}>
+                      {rankData.sharingCount}회
+                    </span>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
