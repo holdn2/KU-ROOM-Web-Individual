6d760fe479f41f44e81378fa414e06ef7dfff081
fr0gydev <ljh130334@naver.com>
2026-02-14 10:29:41 +0900

    style: PwaGuide CSS를 CSS module로 변환

--- src/pages/PwaGuide/PwaGuide.tsx
+++ src/pages/PwaGuide/PwaGuide.tsx
@@ -3,30 +3,30 @@ import section1 from "@assets/pwa-guide/section1.png";
 import section2 from "@assets/pwa-guide/section2.png";
 
 import { usePwaGuide } from "./hooks/usePwaGuide";
-import "./PwaGuide.css";
+import styles from "./PwaGuide.module.css";
 
 const PwaGuide = () => {
   const { handleDismiss } = usePwaGuide();
 
   return (
-    <div className="pwa-guide-page">
-      <div className="pwa-guide-header">
-        <img src={cloudLogo} alt="쿠룸 로고" className="pwa-guide-logo" />
-        <h1 className="pwa-guide-title">
+    <div className={styles.Page}>
+      <div className={styles.Header}>
+        <img src={cloudLogo} alt="쿠룸 로고" className={styles.Logo} />
+        <h1 className={styles.Title}>
           홈 화면에 쿠룸을 추가하고
           <br />
           이용해주세요!
         </h1>
-        <p className="pwa-guide-subtitle">
+        <p className={styles.Subtitle}>
           설치 환경에 따른 방법을 안내드려요.
         </p>
       </div>
 
-      <img src={section1} alt="Safari 설치 가이드" className="pwa-guide-section-img" />
-      <img src={section2} alt="Chrome 설치 가이드" className="pwa-guide-section-img" />
+      <img src={section1} alt="Safari 설치 가이드" className={styles.SectionImg} />
+      <img src={section2} alt="Chrome 설치 가이드" className={styles.SectionImg} />
 
-      <div className="pwa-guide-bottom">
-        <span className="pwa-guide-dismiss-text" onClick={handleDismiss}>
+      <div className={styles.Bottom}>
+        <span className={styles.DismissText} onClick={handleDismiss}>
           이미 설치했어요
         </span>
       </div>
